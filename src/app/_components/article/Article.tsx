c9be9e09572a62918f792d5270a5c137da4598c4
Luke <luke@lpdev.io>
2024-12-03 01:51:31 +1100

    chore: safety checks, remove unnecessary code

--- src/app/_components/article/Article.tsx
+++ src/app/_components/article/Article.tsx
@@ -25,21 +25,6 @@ export default async function Article({ ...article }: ArticleData) {
           </h1>
           <h2 className="text-lg italic md:text-xl">{article.subheading}</h2>
         </div>
-
-        <div className="mt-6 flex-shrink-0 text-[0.9rem] sm:ml-6 sm:mt-0 sm:text-right">
-          <p>
-            <span className="font-medium text-gray-800">created:</span>{' '}
-            {/* <span className="text-gray-500"> */}
-            {/*   {formatDate(article._createdAt)} */}
-            {/* </span> */}
-          </p>
-          <p>
-            <span className="font-medium text-gray-800">updated:</span>{' '}
-            {/* <span className="text-gray-500"> */}
-            {/*   {formatDate(article._updatedAt)} */}
-            {/* </span> */}
-          </p>
-        </div>
       </div>
 
       {/* <PortableText */}
--- src/app/_components/article/ArticleList.tsx
+++ src/app/_components/article/ArticleList.tsx
@@ -4,6 +4,8 @@ import { getAllArticles } from '@/utils/contentful/api';
 export default async function ArticleList() {
   const articles = await getAllArticles();
 
+  if (!articles || articles.length < 1) return null;
+
   return (
     <>
       {articles.map((article) => (
